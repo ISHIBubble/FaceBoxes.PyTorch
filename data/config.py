d554fc693ab947d5bcfab788b7a8974b0a1e168d
ISHIBubble <ishai.bla@gmail.com>
2025-12-23 14:41:10 +0200

    ready for Gaudi

--- data/config.py
+++ data/config.py
@@ -1,6 +1,6 @@
 # config.py
 
-cfg = {
+'''cfg = {
     'name': 'FaceBoxes',
     #'min_dim': 1024,
     #'feature_maps': [[32, 32], [16, 16], [8, 8]],
@@ -11,4 +11,16 @@ cfg = {
     'clip': False,
     'loc_weight': 2.0,
     'gpu_train': True
+}'''
+cfg = {
+    'name': 'FaceBoxes',
+    #'min_dim': 1024,
+    #'feature_maps': [[32, 32], [16, 16], [8, 8]],
+    # 'aspect_ratios': [[1], [1], [1]],
+    'min_sizes': [[32, 64, 128], [256], [512]],
+    'steps': [32, 64, 128],
+    'variance': [0.1, 0.2],
+    'clip': False,
+    'loc_weight': 2.0,
+    'gpu_train': False
 }
