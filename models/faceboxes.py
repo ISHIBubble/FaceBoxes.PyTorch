1730dbde0b1a82e1e63716b09fa4572057a6d65b
Ishai Bleiweiss <ishai.bla@gmail.com>
2026-01-14 09:42:19 +0200

    changed activation func to hardswish

--- models/faceboxes.py
+++ models/faceboxes.py
@@ -13,7 +13,8 @@ class BasicConv2d(nn.Module):
     def forward(self, x):
         x = self.conv(x)
         x = self.bn(x)
-        return F.relu(x, inplace=True)
+        # return F.relu(x, inplace=True)
+        return F.hardswish(x, inplace=True)
 
 
 class Inception(nn.Module):
@@ -56,7 +57,8 @@ class CRelu(nn.Module):
     x = self.conv(x)
     x = self.bn(x)
     x = torch.cat([x, -x], 1)
-    x = F.relu(x, inplace=True)
+    # x = F.relu(x, inplace=True)
+    x = F.hardswish(x, inplace=True)
     return x
 
 
